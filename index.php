627f023c722b192053618ee76b88497db4504585
khusnul188 <khusnulmubaroq8@gmail.com>
2023-11-14 10:59:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Rumah Makan</title>
+</head>
+
+<body>
+    <header>
+        <h1>Rumah Makanan</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a><li>/
+                <li><a href="#">Hot Promo</a><li>
+                <li><a href="#">Makanan</a><li>
+                <li><a href="#">Minuman</a><li>''
+</ul>
+</nav>
+</header>
+<section class="menu">
+    <h2>Menu Makanan</h2>
+    <div class="menu-item">
+        <img src="img/bakso.jpg" alt="menu 1">
+        <h3>Bakso Granat</h3>
+        <p>Deskripsi tentang makanan misalnya nilai gizi dsb.</p>
+        <p class="harga">Harga : Rp: 10.000</p>
+        <button class="order-button">pesan</button>
+</div>
+    <div class="menu-item">
+        <img src="img/bakso.jpg" alt="menu 1">
+        <h3>Bakso Granat</h3>
+        <p>Deskripsi tentang makanan misalnya nilai gizi dsb.</p>
+        <p class="harga">Harga : Rp: 10.000</p>
+        <button class="order-button">pesan</button>
+</div>
+    <div class="menu-item">
+        <img src="img/bakso.jpg" alt="menu 1">
+        <h3>Bakso Granat</h3>
+        <p>Deskripsi tentang makanan misalnya nilai gizi dsb.</p>
+        <p class="harga">Harga : Rp: 10.000</p>
+        <button class="order-button">pesan</button>
+</div>
+    <div class="menu-item">
+        <img src="img/bakso.jpg" alt="menu 1">
+        <h3>Bakso Granat</h3>
+        <p>Deskripsi tentang makanan misalnya nilai gizi dsb.</p>
+        <p class="harga">Harga : Rp: 10.000</p>
+        <button class="order-button">pesan</button>
+</div>
+</section>
+<section class="contact">
+    <h2>Hubungi Kami</h2>
+    <p>Jika ada pertayataan, saran atau kritik hubungi kami di:</p>
+    <p>WA 085232618202</p>
+    <p>Email : khusnulmubaroq@gmail.com</p>
+</section>
+<footer>
+    <p>&copy; 2023 Restoran Enak .Hak cipta di lindungi</p>
+</footer>
+
+</body>
+</html>
